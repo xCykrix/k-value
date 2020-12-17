cff980174be69f79965fb995a710f68b81d34835
Samuel J Voeller <samuel.voeller@amethyst.live>
2020-12-17 06:01:09 +0100

    feat: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      impliedStrict: true
+    },
+    project: './tsconfig.json'
+  },
+  plugins: [
+    '@typescript-eslint',
+    'eslint-plugin-tsdoc'
+  ],
+  extends: [
+    'eslint:recommended',
+    'standard-with-typescript'
+  ],
+  ignorePatterns: [
+    '/dist/'
+  ],
+  env: {
+    mocha: true
+  },
+  rules: {
+    'tsdoc/syntax': 'warn'
+  }
+}
